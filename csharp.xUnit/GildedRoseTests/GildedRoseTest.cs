8797a81d0a3c14c2e42f0cfdee057b3ca96a8752
Flash <joshuaflash@gmail.com>
2026-09-17 11:42:18 +1200

    Add unit tests for quality and sell-in decrementing along with re-usable test item data to GildedRoseTest

--- csharp.xUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.xUnit/GildedRoseTests/GildedRoseTest.cs
@@ -6,14 +6,42 @@ namespace GildedRoseTests;
 
 public class GildedRoseTest
 {
-    private const string TestItemName = "TestItem";
-    
-    [Fact]
-    public void WhenUpdatingQuality_ItemNameIsUnaltered()
+    private class DefaultItemData : TheoryData<Item>
     {
-        IList<Item> Items = new List<Item> { new Item { Name = TestItemName, SellIn = 0, Quality = 0 } };
-        GildedRose app = new GildedRose(Items);
+        public DefaultItemData() => AddRange(
+            new Item { Name = "TestItem", SellIn = 1, Quality = 1}
+        );
+    }
+    
+    [Theory]
+    [ClassData(typeof(DefaultItemData))]
+    public void WhenUpdatingQuality_ItemNameIsUnaltered(Item item)
+    {
+        IList<Item> items = new List<Item> { item };
+        GildedRose app = new GildedRose(items);
         app.UpdateQuality();
-        Assert.Equal(TestItemName, Items[0].Name);
+        Assert.Equal(item.Name, items[0].Name);
+    }
+    
+    [Theory]
+    [ClassData(typeof(DefaultItemData))]
+    public void WhenUpdatingQuality_ItemSellInIsCorrectlyDecremented(Item item)
+    {
+        int expectedSellIn = item.SellIn - 1;
+        IList<Item> items = new List<Item> { item };
+        GildedRose app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.Equal(expectedSellIn, items[0].SellIn);
+    }
+    
+    [Theory]
+    [ClassData(typeof(DefaultItemData))]
+    public void WhenUpdatingQuality_ItemQualityIsCorrectlyDecremented(Item item)
+    {
+        int expectedQuality = item.Quality > 0 ? item.Quality - 1 : 0;
+        IList<Item> items = new List<Item> { item };
+        GildedRose app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.Equal(expectedQuality, items[0].Quality);
     }
 }

19f4e2ebd389f2942046d8928c33283fd2ef3619
Flash <joshuaflash@gmail.com>
2026-09-17 12:08:45 +1200

    extend QualityEvaluator to correctly implement evaluation functions for different item quality behaviours

--- csharp.xUnit/GildedRose/QualityEvaluator.cs
+++ csharp.xUnit/GildedRose/QualityEvaluator.cs
@@ -1,8 +1,31 @@
-﻿namespace GildedRoseKata;
+﻿using System;
+
+namespace GildedRoseKata;
 
 public static class QualityEvaluator
 {
+    private const int MaxQuality = 50;
+    private const int MinQuality = 0;
+
     public delegate int QualityEvaluation(int sellIn, int quality);
-    
-    public static QualityEvaluation DefaultDepreciation = (sellIn, quality) => sellIn > 0 ? quality - 1 : quality - 2;
+
+    public static QualityEvaluation DefaultDepreciation = (sellIn, quality) =>
+        Math.Clamp(sellIn > 0 ? quality - 1 : quality - 2, MinQuality, MaxQuality);
+
+    public static QualityEvaluation Static = (_, quality) => Math.Max(MinQuality, quality);
+
+    public static QualityEvaluation DefaultAppreciation =
+        (_, quality) => Math.Clamp(quality + 1, MinQuality, MaxQuality);
+
+    public static QualityEvaluation LimitedTimeOnly = (sellIn, quality) =>
+    {
+        var result = sellIn <= 0 ? 0
+            : sellIn > 10 ? quality + 1
+            : sellIn > 5 ? quality + 2
+            : quality + 3;
+
+        return Math.Clamp(result, MinQuality, MaxQuality);
+    };
+
+    public static QualityEvaluation ConjuredDepreciation = (sellIn, quality) => throw new NotImplementedException();
 }
